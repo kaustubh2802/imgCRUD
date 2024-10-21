08a4b5b8832bb65ba6531e4ad933480f8ea6e6c8
kaustubh2802 <kbnitro5@gmail.com>
2024-10-21 17:04:06 +0530

    crud working

--- application/views/student_view.php
+++ application/views/student_view.php
@@ -84,6 +84,7 @@
     <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.min.js"></script> -->
+
     <script>
         $(document).ready(function() {
             loadStudents();
@@ -189,6 +190,7 @@
                            $('#editModal').modal('hide');
                            loadStudents();
                         //    sweetalert2
+                        
                         Swal.fire("Data Updated Successfully!");
 
                            alert('Student updated successfully!');
